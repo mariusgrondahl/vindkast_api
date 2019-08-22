3628fc69c17d0e6c7beab292cc40f074c66c10fc
Marius Grondahl <marius@grondahldesign.no>
2019-08-22 14:03:29 +0200

    Added userroutes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/userModel");
 
 router.get("/:id", (req,res,next)=> {
-    Marker.findById(req.params.id, function(err, marker) {
+    User.findById(req.params.id, function(err, user) {
       if (err)
         res.send(err);
-      res.json(marker);
+      res.json(user);
     });
 })
 
+
+router.get("/:id/delete", (req,res,next)=>  {
+    User.remove({
+        _id: req.params.id
+      }, function(err, user) {
+        if (err)
+          res.send(err);
+        res.json({ message: 'User successfully deleted' });
+      });
+})
+  
 module.exports = router;
